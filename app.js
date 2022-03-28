3df7587990c17d3bb887bff1e20e26e6bfca81eb
Parvez Mia <parvez.pz1971@gmail.com>
2022-03-28 16:34:49 +0600

    done with mongodb atomic query

--- app.js
+++ app.js
@@ -4,73 +4,91 @@ import Submission from './models/submission.js';
 import  './judge.js';
 import judgeCpp from './judge.js';
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true, useUnifiedTopology: true
-// })
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+// cpu core lock flag
+var lock = [0, 0, 0, 0];
+
+const createSubmission = async (filename) => {
+    await Submission.create({source:filename, status: 'on_queue'});
+    console.log("submission created");
+    await startJudging();
+}
+
+const startJudging = async () => {
+    // find and modify one document from the collection with status on_queue
+    const submission = await Submission.findOneAndUpdate({status: 'on_queue'}, {status: 'judging'}, {new: true});
+    if (submission) {
+        // console.log("submission found");
+        // assign cpu core
+        var cpu = 0;
+        while (1) {
+            if (lock[cpu] == 0) {
+                lock[cpu] = 1;
+                break;
+            } else {
+                cpu = (cpu + 1) % 4;
+            }
+            // sleep for 100ms
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        // judge the submission
+        await judgeCpp(submission.source, cpu);
+        console.log(`judging: ${submission.source}`);
+        // release cpu core
+        lock[cpu] = 0;
+        // update the status of the submission
+        var x = await Submission.findOneAndUpdate({source: submission.source}, {status: 'AC'}, {new: true});
+        console.log(`submission ${x.source} is AC`);
+    }
+}
+
+createSubmission('./OJ/solution.cpp');
+createSubmission('./OJ/WA_solution.cpp');
+createSubmission('./OJ/TLE_solution.cpp');
+createSubmission('./OJ/MLE_solution.cpp');
+
 
 // // cpu core lock flag
 // var lock = [0, 0, 0, 0];
 // var assignCpu = 3 ;
 
-// const createSubmission = async (filename) => {
-//     await Submission.create({source:filename, status: 'on_queue'});
-//     console.log("submission created");
-//     await startJudging();
-// }
+// import Queue from 'bull';
+// const judgeQueue = new Queue('judge');
 
-// const startJudging = async () => {
-//     // find one on_queue submission
-//     const submission = await Submission.findOne({status: 'on_queue'});
+// // producer
+// await judgeQueue.add( { filename: './OJ/solution.cpp' } );
+// // await judgeQueue.add( { filename: './OJ/WA_solution.cpp' } );
+// // await judgeQueue.add( { filename: './OJ/TLE_solution.cpp' } );
+// // await judgeQueue.add( { filename: './OJ/MLE_solution.cpp' } );
 
-//     // if no submission found, return
-//     if(!submission) return;
-    
-//     // save submission status to judging
-//     await submission.update({status: 'judging'});
-//     assignCpu = (assignCpu + 1) % 4;
-    
-//     // wait until lock[assignCpu] = 0
-//     while(lock[assignCpu] == 1) {
-//         await sleep(100);
-//     }
+// console.log('hello');
 
-//     // lock cpu core
-//     lock[assignCpu] = 1;
-//     await judgeCpp(submission.source, 3, 10000, assignCpu);
-//     lock[assignCpu] = 0;
-//     await submission.update({status: 'AC'});
-//     console.log("submission judged");
-// }
+// // consumer
+// const judge = judgeQueue.process(async (job) => {
+//     // assignCpu = (assignCpu + 1) % 4;
+//     // // wait until lock[assignCpu] = 0
+//     // while(lock[assignCpu] == 1) {
+//     //     await sleep(100);
+//     // }
+//     return judgeCpp(job.data.filename, 3, 10000, assignCpu);
+//   });
 
-// createSubmission('./OJ/solution.cpp');
-// createSubmission('./OJ/WA_solution.cpp');
-// createSubmission('./OJ/TLE_solution.cpp');
-// createSubmission('./OJ/MLE_solution.cpp');
+// await judge();
 
+// import Queue  from 'bee-queue';
+// const queue = new Queue('example');
 
-// cpu core lock flag
-var lock = [0, 0, 0, 0];
-var assignCpu = 3 ;
+// const job = queue.createJob({x: 2, y: 3});
+// job.save();
+// job.on('succeeded', (result) => {
+//   console.log(`Received result for job ${job.id}: ${result}`);
+// });
 
-import Queue from 'bull';
-const judgeQueue = new Queue('judge');
-
-// producer
-await judgeQueue.add( { filename: './OJ/solution.cpp' } );
-// await judgeQueue.add( { filename: './OJ/WA_solution.cpp' } );
-// await judgeQueue.add( { filename: './OJ/TLE_solution.cpp' } );
-// await judgeQueue.add( { filename: './OJ/MLE_solution.cpp' } );
-
-console.log('hello');
-
-// consumer
-const judge = judgeQueue.process(async (job) => {
-    // assignCpu = (assignCpu + 1) % 4;
-    // // wait until lock[assignCpu] = 0
-    // while(lock[assignCpu] == 1) {
-    //     await sleep(100);
-    // }
-    return judgeCpp(job.data.filename, 3, 10000, assignCpu);
-  });
-
-await judge();
+// // Process jobs from as many servers or processes as you like
+// queue.process(function (job, done) {
+//   console.log(`Processing job ${job.id}`);
+//   return done(null, job.data.x + job.data.y);
+// });
